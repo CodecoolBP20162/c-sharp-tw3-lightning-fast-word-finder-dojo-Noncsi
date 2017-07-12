06add2a807ab236edabec74711cd8ed8da12dd1d
Sheiwa <lengyel.noemi96@gmail.com>
2017-07-12 10:55:48 +0200

    LightningWordFinder Dojo

--- LightningFastWordFinder/LightningWordFinder.cs
+++ LightningFastWordFinder/LightningWordFinder.cs
@@ -4,9 +4,24 @@ namespace LightningFastWordFinder
 {
     class LightningWordFinder
     {
+        string longestWord;
         public string GetLongestWord(string text)
         {
-            return "longestWord";
+            int NumberOfCharactersOfYetLongestWord = 0;
+            string[] stringArray = text.Split(new string[] { " ", ".", "?", "!" }, StringSplitOptions.None);
+            foreach (string word in stringArray)
+            {
+                for(int j = 0; j <= word.Length; j++)
+                {
+                    if (NumberOfCharactersOfYetLongestWord < j)
+                    {
+                        NumberOfCharactersOfYetLongestWord = word.Length;
+                        longestWord = word;
+                        break;
+                    }
+                }
+            }
+            return longestWord;
         }
     }
 }
